ba82ab935b2d99a39fbe3f6199b219340b7c5f55
token <239573049@qq.com>
2024-02-01 00:57:50 +0800

    增加项目

---
+++ src/Gateway.Core/IStaticFileProxyService.cs
@@ -0,0 +1,17 @@
+﻿using Gateway.Core.Entities;
+using Gateway.Dto;
+
+namespace Gateway.Core;
+
+public interface IStaticFileProxyService
+{
+    Task RefreshConfig();
+    
+    Task<ResultDto> CreateAsync(StaticFileProxyEntity entity);
+    
+    Task<ResultDto> UpdateAsync(StaticFileProxyEntity entity);
+    
+    Task<ResultDto> DeleteAsync(string id);
+    
+    Task<PageResultDto<StaticFileProxyEntity>> GetListAsync(string keyword);
+}
---
+++ src/plugins/Gateway.Plugins/IGatewayPlugins.cs
@@ -0,0 +1,41 @@
+﻿namespace Gateway.Plugins;
+
+public interface IGatewayPlugins
+{
+    
+    /// <summary>
+    /// 获取插件信息
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<PluginInfo> GetPluginInfoAsync(CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// 初始化插件
+    /// </summary>
+    /// <param name="serviceProvider"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task InitializeAsync(IServiceProvider serviceProvider, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// 开启插件
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task StartAsync(CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// 停止插件
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task StopAsync(CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// 卸载插件
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task UninitializeAsync(CancellationToken cancellationToken = default);
+}
