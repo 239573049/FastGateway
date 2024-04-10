7303862e53820b3ded72daa4e6d69134cbfc1fe0
token <239573049@qq.com>
2024-04-11 01:36:50 +0800

    增加限流设计

---
+++ src/FastGateway.Core/RateLimitType.cs
@@ -0,0 +1,24 @@
+﻿namespace FastGateway.Core;
+
+public enum RateLimitType : byte
+{
+    /// <summary>
+    /// 并发策略
+    /// </summary>
+    Concurrent = 1,
+    
+    /// <summary>
+    /// 令牌桶
+    /// </summary>
+    TokenBucket = 2,
+    
+    /// <summary>
+    /// 滑动窗口
+    /// </summary>
+    SlidingWindow = 3,
+    
+    /// <summary>
+    /// 固定窗口
+    /// </summary>
+    FixedWindow = 4
+}
---
+++ src/FastGateway/Domain/RateLimit.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.RateLimiting;
+
+namespace FastGateway.Domain;
+
+public sealed class RateLimit
+{
+    public RateLimitType Type { get; set; }
+
+    #region 固定窗口限制器
+
+    public int? PermitLimit { get; set; }
+
+    /// <summary>
+    /// 限流时间窗口
+    /// </summary>
+    public TimeSpan? Window { get; set; }
+
+    /// <summary>
+    /// QueueProcessingOrder
+    /// </summary>
+    public QueueProcessingOrder? QueueProcessingOrder { get; set; }
+
+    /// <summary>
+    /// QueueLimit
+    /// </summary>
+    public int? QueueLimit { get; set; }
+
+    #endregion
+
+    #region 滑动窗口限制器
+
+    /// <summary>
+    /// SegmentsPerWindow 
+    /// </summary>
+    public int? SegmentsPerWindow { get; set; }
+
+    #endregion
+
+    #region 令牌桶限制器
+
+    /// <summary>
+    /// TokenLimit
+    /// </summary>
+    public int? TokenLimit { get; set; }
+
+    /// <summary>
+    /// TokensPerPeriod
+    /// </summary>
+    public int? TokensPerPeriod { get; set; }
+
+    /// <summary>
+    /// AutoReplenishment
+    /// </summary>
+    public bool? AutoReplenishment { get; set; }
+
+    #endregion
+
+    /// <summary>
+    /// 拒绝响应状态码
+    /// </summary>
+    public int RejectionStatusCode { get; set; }
+}
