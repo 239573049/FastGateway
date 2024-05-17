e7a097eca274331295b38fedce76c54aa8aff908
hejiale010426 <>
2024-05-18 03:39:56 +0800

    增加Console版本

---
+++ src/FastGateway.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using FastGateway.TunnelClient;
+
+var builder = WebApplication.CreateBuilder();
+
+var gateway= builder.Configuration["Gateway"];
+
+if (string.IsNullOrEmpty(gateway))
+{
+    Console.WriteLine("Gateway is empty");
+    return;
+}
+
+builder.WebHost.UseTunnelTransport(gateway);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+app.MapReverseProxy();
+
+await app.RunAsync();
