086c168e7432c1c2b001659fc3e43c4415f8f690
fly <luxiaofeng006@qq.com>
2024-06-11 16:54:37 +0800

    [bug fix] HasHttpService 是 静态全局变量，被当成 局部变量 使用了，导致 非80端口 在 启用SSL 时会绑定 443，443被重复绑定导致app启动失败； [feature]  非80端口 在 启用SSL 时，启用 http和https单端口双协议服务器。

---
+++ src/FastGateway/Middlewares/TlsDetection/TlsRestoreMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Http.Features;
+using System.Threading.Tasks;
+
+namespace KestrelFramework.Middleware.TlsDetection
+{
+    /// <summary>
+    /// tls恢复中间件
+    /// </summary>
+    sealed class TlsRestoreMiddleware : IKestrelMiddleware
+    {
+        /// <summary>
+        /// 执行中间件
+        /// </summary>
+        /// <param name="next"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task InvokeAsync(ConnectionDelegate next, ConnectionContext context)
+        {
+            if (context.Features.Get<ITlsConnectionFeature>() == FakeTlsConnectionFeature.Instance)
+            {
+                // 擦除入侵
+                context.Features.Set<ITlsConnectionFeature>(null);
+            }
+            await next(context);
+        }
+    }
+}
