6253008c0cfca84221400fcd30d0b1a0c9146e7c
token <239573049@qq.com>
2024-11-26 01:57:07 +0800

    增加异常拦截

--- src/FastGateway.Service/Program.cs
+++ src/FastGateway.Service/Program.cs
@@ -19,140 +19,146 @@ public static class Program
 {
     public static async Task Main(string[] args)
     {
-        Directory.SetCurrentDirectory(AppContext.BaseDirectory);
-
-        var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+        try
         {
-            ContentRootPath = AppContext.BaseDirectory,
-            Args = args
-        });
+            Directory.SetCurrentDirectory(AppContext.BaseDirectory);
 
-        var logger = new LoggerConfiguration()
-            .WriteTo.Console()
-            .CreateLogger();
-
-        builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(JwtOptions.Name));
-
-        builder.Host.UseSerilog(logger);
-        builder.Services.AddHttpClient();
-
-        // 判断是否window，
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            builder.Services.AddWindowsService(option =>
+            var builder = WebApplication.CreateBuilder(new WebApplicationOptions
             {
-                option.ServiceName = "FastGateway";
+                ContentRootPath = AppContext.BaseDirectory,
+                Args = args
             });
-        }
 
-        var jwtOptions = builder.Configuration.GetSection(JwtOptions.Name).Get<JwtOptions>();
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
 
-        builder.Services
-            .AddAuthorization()
-            .AddAuthentication((options => { options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme; }))
-            .AddJwtBearer((options =>
+            builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(JwtOptions.Name));
+
+            builder.Host.UseSerilog(logger);
+            builder.Services.AddHttpClient();
+
+            // 判断是否window，
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                options.TokenValidationParameters = new TokenValidationParameters
+                builder.Services.AddWindowsService(option => { option.ServiceName = "FastGateway"; });
+            }
+
+            var jwtOptions = builder.Configuration.GetSection(JwtOptions.Name).Get<JwtOptions>();
+
+            builder.Services
+                .AddAuthorization()
+                .AddAuthentication((options => { options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme; }))
+                .AddJwtBearer((options =>
                 {
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ClockSkew = TimeSpan.FromDays(jwtOptions.ExpireDay),
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Secret))
-                };
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ClockSkew = TimeSpan.FromDays(jwtOptions.ExpireDay),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Secret))
+                    };
+                }));
+
+            builder.Services.AddAutoGnarly();
+
+            builder.Services.ConfigureHttpJsonOptions(options =>
+            {
+                options.SerializerOptions.Converters.Add(new DateTimeJsonConverter());
+            });
+            builder.Services.AddSystemUsage();
+
+            builder.Services.AddResponseCompression();
+            builder.Services.AddSingleton<ISearcher>(new Searcher(CachePolicy.File, "ip2region.xdb"));
+            builder.Services.AddHostedService<LoggerBackgroundTask>();
+            builder.Services.AddHostedService<RenewSSLBackgroundService>();
+            builder.Services.AddHostedService<ClientRequestBackgroundTask>();
+            builder.Services.AddHostedService<LogCleaningBackgroundService>();
+            builder.Services.AddDbContext<MasterContext>(optionsBuilder =>
+            {
+                // 判断当前目录是否存在data文件夹
+                if (!Directory.Exists("./data"))
+                {
+                    Directory.CreateDirectory("./data");
+                }
+
+                optionsBuilder.UseSqlite(builder.Configuration["Master"])
+                    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            builder.Services.AddDbContext<LoggerContext>(optionsBuilder =>
+            {
+                // 判断当前目录是否存在data文件夹
+                if (!Directory.Exists("./data"))
+                {
+                    Directory.CreateDirectory("./data");
+                }
+
+                optionsBuilder.UseSqlite(builder.Configuration["Logger"])
+                    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var loggerContext = scope.ServiceProvider.GetRequiredService<LoggerContext>();
+                var dbContext = scope.ServiceProvider.GetRequiredService<MasterContext>();
+                await dbContext.Database.MigrateAsync();
+                await loggerContext.Database.MigrateAsync();
+
+                CertService.InitCert(await dbContext.Certs.ToArrayAsync());
+
+                var domainNames = await dbContext.DomainNames.ToArrayAsync();
+                var blacklistAndWhitelists = await dbContext.BlacklistAndWhitelists.ToArrayAsync();
+                var rateLimits = await dbContext.RateLimits.ToArrayAsync();
+                foreach (var item in await dbContext.Servers.ToArrayAsync())
+                {
+                    await Task.Factory.StartNew(async () =>
+                        await Gateway.Gateway.BuilderGateway(item,
+                            domainNames.Where(x => x.ServerId == item.Id).ToArray(),
+                            blacklistAndWhitelists, rateLimits));
+                }
+            }
+
+            app.Use((async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
             }));
 
-        builder.Services.AddAutoGnarly();
+            app.UseResponseCompression();
 
-        builder.Services.ConfigureHttpJsonOptions(options =>
-        {
-            options.SerializerOptions.Converters.Add(new DateTimeJsonConverter());
-        });
-        builder.Services.AddSystemUsage();
-        
-        builder.Services.AddResponseCompression();
-        builder.Services.AddSingleton<ISearcher>(new Searcher(CachePolicy.File, "ip2region.xdb"));
-        builder.Services.AddHostedService<LoggerBackgroundTask>();
-        builder.Services.AddHostedService<RenewSSLBackgroundService>();
-        builder.Services.AddHostedService<ClientRequestBackgroundTask>();
-        builder.Services.AddHostedService<LogCleaningBackgroundService>();
-        builder.Services.AddDbContext<MasterContext>(optionsBuilder =>
-        {
-            // 判断当前目录是否存在data文件夹
-            if (!Directory.Exists("./data"))
-            {
-                Directory.CreateDirectory("./data");
-            }
+            app.UseStaticFiles();
 
-            optionsBuilder.UseSqlite(builder.Configuration["Master"])
-                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-        });
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-        builder.Services.AddDbContext<LoggerContext>(optionsBuilder =>
-        {
-            // 判断当前目录是否存在data文件夹
-            if (!Directory.Exists("./data"))
-            {
-                Directory.CreateDirectory("./data");
-            }
+            app.MapDomain()
+                .MapBlacklistAndWhitelist()
+                .MapCert()
+                .MapSetting()
+                .MapApiQpsService()
+                .MapFileStorage()
+                .MapRateLimit()
+                .MapAuthorizationService()
+                .MapDashboard()
+                .MapApplicationLogger()
+                .MapServer();
 
-            optionsBuilder.UseSqlite(builder.Configuration["Logger"])
-                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-        });
-
-        var app = builder.Build();
-
-        using (var scope = app.Services.CreateScope())
-        {
-            var loggerContext = scope.ServiceProvider.GetRequiredService<LoggerContext>();
-            var dbContext = scope.ServiceProvider.GetRequiredService<MasterContext>();
-            await dbContext.Database.MigrateAsync();
-			await loggerContext.Database.MigrateAsync();
-
-            CertService.InitCert(await dbContext.Certs.ToArrayAsync());
-
-            var domainNames = await dbContext.DomainNames.ToArrayAsync();
-            var blacklistAndWhitelists = await dbContext.BlacklistAndWhitelists.ToArrayAsync();
-            var rateLimits = await dbContext.RateLimits.ToArrayAsync();
-            foreach (var item in await dbContext.Servers.ToArrayAsync())
-            {
-                await Task.Factory.StartNew(async () =>
-                    await Gateway.Gateway.BuilderGateway(item, domainNames.Where(x => x.ServerId == item.Id).ToArray(),
-                        blacklistAndWhitelists, rateLimits));
-            }
+            await app.RunAsync();
         }
-
-        app.Use((async (context, next) =>
+        catch (Exception e)
         {
-            await next();
-
-            if (context.Response.StatusCode == 404)
-            {
-                context.Request.Path = "/index.html";
-                await next();
-            }
-        }));
-
-        app.UseResponseCompression();
-
-        app.UseStaticFiles();
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapDomain()
-            .MapBlacklistAndWhitelist()
-            .MapCert()
-            .MapSetting()
-            .MapApiQpsService()
-            .MapFileStorage()
-            .MapRateLimit()
-            .MapAuthorizationService()
-            .MapDashboard()
-            .MapApplicationLogger()
-            .MapServer();
-
-        await app.RunAsync();
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
